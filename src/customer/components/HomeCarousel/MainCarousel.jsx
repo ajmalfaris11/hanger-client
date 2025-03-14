520c23cf177a301d8c5575bdf2a73aec4bbe0b44
Ajmal Faris <ajmalfaris.me@gmail.com>
2025-03-15 03:35:27 +0530

    refactor: Load carousel items from external data file and enable autoPlay

--- src/customer/components/HomeCarousel/MainCarousel.jsx
+++ src/customer/components/HomeCarousel/MainCarousel.jsx
@@ -1,22 +1,26 @@
-import React from 'react';
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import { mainCarouselData } from "./MainCarouselData";
 
-
-const items = [
-    <div className="item" data-value="1">1</div>,
-    <div className="item" data-value="2">2</div>,
-    <div className="item" data-value="3">3</div>,
-    <div className="item" data-value="4">4</div>,
-    <div className="item" data-value="5">5</div>,
-];
-
-const  MainCarousel = () => (
-    <AliceCarousel
-        mouseTracking
-        items={items}
-        controlsStrategy="alternate"
+const MainCarousel = () => {
+  const items = mainCarouselData.map((item) => (
+    <img
+      className="cursor-pointer"
+      role="presentation"
+      src={item.image}
+      alt="main-carousel"
     />
-);
+  ));
+
+  return (
+    <AliceCarousel 
+    items={items}
+    disableButtonsControls
+    autoPlay
+    autoPlayInterval={1000}
+    />
+  )
+};
 
 export default MainCarousel;
