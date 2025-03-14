d823fe3b820e997715a589dcac734c27c983274a
Ajmal Faris <ajmalfaris.me@gmail.com>
2025-03-15 02:33:44 +0530

    git commit -m "feat: Add HomePage component to App"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navigation from './customer/components/Navbar/Navigation';
+import HomePage from './customer/Pages/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
         <Navigation/>
+        <div>
+          <HomePage/>
+        </div>
     </div>
   );
 }
