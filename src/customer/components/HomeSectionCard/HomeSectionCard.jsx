fcfa242eb4fe8687dc0b159c4eadd6e615fe76ae
Ajmal Faris <ajmalfaris.me@gmail.com>
2025-03-20 12:00:00 +0530

    Enhance carousel functionality with navigation controls and dynamic data binding

--- src/customer/components/HomeSectionCard/HomeSectionCard.jsx
+++ src/customer/components/HomeSectionCard/HomeSectionCard.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function HomeSectionCard({product}) {
   return (
-    <div className="group cursor-pointer flex flex-col items-center rounded-xl shadow-md overflow-hidden mx-2 mb-10 transition-transform duration-300 transform hover:scale-105 bg-white">
+    <div className="group cursor-pointer flex flex-col items-center rounded-xl shadow-md overflow-hidden mx-2 mb-10 transition-transform duration-300 transform hover:scale-105 bg-white mt-2">
     <div className="h-[14rem] w-full bg-gray-100 rounded-t-xl overflow-hidden">
-        <img className="object-cover object-top w-full h-full transition-opacity duration-300 group-hover:opacity-90" 
+        <img className="object-cover object-top w-full h-full transition-opacity duration-300 group-hover:opacity-95" 
              src={product.imageUrl} 
              alt={product.title} />
     </div>
--- src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import AliceCarousel from "react-alice-carousel";
 import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { mens_kurta } from "../../Data/mens_kurta";
+import { button } from "@mui/material";
 
 const HomeSectionCarousel = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const carouselItems = mens_kurta.map((item) => (
+    <HomeSectionCard product={item} />
+  ));
+  const carouselRef = React.useRef(null);
+
   const responsive = {
     0: { items: 1 },
     568: { items: 2 },
@@ -12,30 +20,47 @@ const HomeSectionCarousel = () => {
     1024: { items: 5 },
   };
 
-  const items = [1, 1, 1, 1, 1].map((item) => <HomeSectionCard />);
+  const slideNext = () => {
+    const newIndex = Math.min(activeIndex + 5, carouselItems.length - 5);
+    setActiveIndex(newIndex);
+    carouselRef.current.slideTo(newIndex);
+  };
+
+  const slidePrev = () => {
+    const newIndex = Math.max(activeIndex - 5, 0);
+    setActiveIndex(newIndex);
+    carouselRef.current.slideTo(newIndex);
+  };
+
   return (
-    <div className="relative py-4 flex flex-row items-center justify-center w-full">
-      <div className="flex items-center">
-        <button className="p-2 bg-white ml-5 mb-20 h-10 text-black rounded-full shadow-md hover:bg-black hover:text-white transition-all duration-200 ease-in-out focus:outline-none">
-          <ChevronLeftIcon />
-        </button>
-      </div>
-      <div className="relative p-4 overflow-x-scroll w-full">
-        <h1 class="text-2xl font-bold text-black mb-4 ml-4">WOMENS </h1>
+    <div className="relative flex flex-row items-center justify-center gap-4 w-full mb-10">
+      <button
+        className="w-14 h-[50px] flex items-center justify-center bg-white ml-5  rounded-full shadow-md hover:bg-black hover:text-white transition-all duration-200 ease-in-out focus:outline-none "
+        onClick={slidePrev}
+        disabled={activeIndex === 0}
+      >
+        <ChevronLeftIcon />
+      </button>
+
+      <div className="relative overflow-hidden w-full">
+        <h1 className="text-2xl font-bold text-black mb-4 ml-4">Men's Kurta</h1>
 
         <AliceCarousel
-          items={items}
+          ref={carouselRef}
+          items={carouselItems}
           disableButtonsControls
-          autoPlay
           responsive={responsive}
           disableDotsControls
         />
       </div>
-      <div className="flex items-center">
-        <button className="p-2 bg-white mr-5 mb-20 h-10 text-black rounded-full shadow-md hover:bg-black hover:text-white transition-all duration-200 ease-in-out focus:outline-none">
-          <ChevronRightIcon />
-        </button>
-      </div>
+
+      <button
+        className="w-14 h-[50px] bg-white mr-5 h-10 text-black rounded-full shadow-md hover:bg-black hover:text-white transition-all duration-200 ease-in-out focus:outline-none"
+        onClick={slideNext}
+        disabled={activeIndex >= carouselItems.length - 1}
+      >
+        <ChevronRightIcon />
+      </button>
     </div>
   );
 };
