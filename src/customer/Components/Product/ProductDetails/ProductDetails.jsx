7f576208a047c0f14b5e4cb9407e74784467b0bd
ajmal faris <ajmalfaris.me@gmail.com>
2025-04-28 17:54:31 +0530

    term and condition page updated

--- src/customer/Components/Product/ProductDetails/ProductDetails.jsx
+++ src/customer/Components/Product/ProductDetails/ProductDetails.jsx
@@ -213,11 +213,11 @@ export default function ProductDetails() {
 
                 <div className="mt-4">
                   <ul role="list" className="list-disc space-y-2 pl-4 text-sm">
-                    {product.highlights.map((highlight) => (
+                    {/* {product.highlights.map((highlight) => (
                       <li key={highlight} className="text-gray-400">
                         <span className="text-gray-600">{highlight}</span>
                       </li>
-                    ))}
+                    ))} */}
                   </ul>
                 </div>
               </div>
