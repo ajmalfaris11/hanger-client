df8a0db068a27953c97da0f01a501d86dbc5526c
ajmal faris <ajmalfaris.me@gmail.com>
2025-04-28 17:17:23 +0530

    deleted demo product details

--- src/customer/Components/Product/ProductDetails/ProductDetails.jsx
+++ src/customer/Components/Product/ProductDetails/ProductDetails.jsx
@@ -14,53 +14,6 @@ import { gounsPage1 } from "../../../../Data/Gouns/gouns";
 import ProductCard from "../ProductCard/ProductCard";
 
 
-const product = {
-  name: "H&M Men's Regular Fit T-shirt",
-  price: "₹2999",
-  href: "#",
-  breadcrumbs: [
-    { id: 1, name: "Men", href: "#" },
-    { id: 2, name: "Clothing", href: "#" },
-  ],
-  images: [
-    {
-      src: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRFf_MKVZsOpipfzf-1wLz4TgRrZvYS9O1yCqrKh0BynMOINziVRo7pGzzhSIPzvLRKq0P6o_-tbRlOmDSs8NvHeQ2TdYVJcWQAiEUt8cij7BAnCYyj1NU9mw",
-      alt: "Two each of gray, white, and black shirts laying flat.",
-    },
-    {
-      src: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQBHE_xM8LIBmdmRUENRryUMP1oO0pA8LWhN9sBhbXbvKZhvCHPmV9tf6Cp-i3432fl7fqYzhnhRMCjZEoBNHKpsusodPJMmBbJ06stQy9OSHVAOu9nYL2v9S4",
-      alt: "Model wearing plain black basic tee.",
-    },
-    {
-      src: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSQ6HHy1OFxeX_Fjpv5OXna8W2NSSI2VT0LwCBjvefvktmKSBbnZ7TON1f7L5CEKmG1wvyCGcBCAWbRSRgKDHooTvXPQjGpC48e-NdCY6CdTIGp8HnHhkLC3w",
-      alt: "Model wearing plain gray basic tee.",
-    },
-    {
-      src: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcRmGXliZp3b-IXETH5YBAQ__mt_dBtEcD3krrfiiFtk6l7pll31rKnN-Wl7zWxNtGm4nQdJWkZ1OAtGbCeZB5cjODk6rEdB-XUqJlPWEPYX9SLsXBzax6D7wA",
-      alt: "Model wearing plain white basic tee.",
-    },
-  ],
-  colors: [
-    { name: "White", class: "bg-white", selectedClass: "ring-gray-400" },
-    { name: "Gray", class: "bg-gray-200", selectedClass: "ring-gray-400" },
-    { name: "Black", class: "bg-gray-900", selectedClass: "ring-gray-900" },
-  ],
-  sizes: [
-    { name: "S", inStock: true },
-    { name: "M", inStock: true },
-    { name: "L", inStock: true },
-  ],
-  description:
-    'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
-  highlights: [
-    "Hand cut and sewn locally",
-    "Dyed with our proprietary colors",
-    "Pre-washed & pre-shrunk",
-    "Ultra-soft 100% cotton",
-  ],
-  details:
-    'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
-};
 
 const reviews = { href: "#", average: 4, totalCount: 117 };
 
